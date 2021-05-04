a3f8b687000ef3a9fa41ce3420cdf7c1de8b79d5
ShadøwXPA <pedro.aves@hotmail.com>
2021-05-04 23:20:54 +0100

    Forgot to remove airplane id

--- ManageAeroSpace/Aviao/main.c
+++ ManageAeroSpace/Aviao/main.c
@@ -28,7 +28,6 @@ int _tmain(int argc, TCHAR** argv, TCHAR** envp) {
 	cfg->airplane.max_capacity = _ttoi(argv[1]);
 	cfg->airplane.velocity = _ttoi(argv[2]);
 	cfg->airplane.airport_start = _ttoi(argv[3]);
-	cfg->airplane.id = cfg->airplane.max_capacity;
 	init_aviao(cfg);
 
 	exit_aviao(cfg);
