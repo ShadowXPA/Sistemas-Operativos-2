ccec8fc4f8ca57075cf84a099347f97efad5d5ba
ShadøwXPA <pedro.aves@hotmail.com>
2021-06-07 12:28:17 +0100

    more lil shet fix

--- ManageAeroSpace/Control/control.c
+++ ManageAeroSpace/Control/control.c
@@ -424,7 +424,7 @@ DWORD WINAPI read_shared_memory(void *param) {
 							airplane->name, airplane->id, airplane->pid, airplane->coordinates.x, airplane->coordinates.y);
 						airplane->airport_start = airplane->airport_end;
 						airplane->airport_end = (const Airport){ 0 };
-						airplane->capacity = 0;
+						//airplane->capacity = 0;
 						buffer.cmd_id |= CMD_OK;
 						buffer.to_id = buffer.from_id;
 						buffer.from_id = 0;
@@ -732,7 +732,7 @@ DWORD WINAPI handle_single_passenger(void *param) {
 		npBuffer.command.airplane = pCfg->passenger->airplane;
 		send_message_namedpipe(pCfg, &npBuffer);
 		SharedBuffer sb;
-		sb.cmd_id = CMD_BOARD;
+		sb.cmd_id = CMD_BOARD | CMD_OK;
 		sb.from_id = 0;
 		sb.to_id = pCfg->passenger->airplane.pid;
 		sb.command.number = 1;
